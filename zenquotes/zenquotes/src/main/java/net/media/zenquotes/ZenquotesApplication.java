edd9c8854330dc2df78e385790ba45e31fd0f402
Mohammad Faizan Ansari <88494801+the-greekgod@users.noreply.github.com>
2023-09-01 13:02:37 +0530

    Basic get and post methods for api

---
+++ zenquotes/zenquotes/src/main/java/net/media/zenquotes/ZenquotesApplication.java
@@ -0,0 +1,15 @@
+package net.media.zenquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenquotesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ZenquotesApplication.class, args);
+
+	}
+
+}
---
+++ zenquotes/zenquotes/src/test/java/net/media/zenquotes/ZenquotesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.media.zenquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenquotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
